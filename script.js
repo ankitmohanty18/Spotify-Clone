c1f3f6ff639f741eab66113d0a40f1d4f6f84828
Ankit <ankitmohanty566@gmail.com>
2023-03-11 19:42:40 +0530

    added broken links

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // console.log("Welcome to spotify");
 
 let songIndex = 1;
-let audioElement = new Audio("1.mp3");
+let audioElement = new Audio("assets/1.mp3");
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("gif");
@@ -11,71 +11,71 @@ let songItems = Array.from(document.getElementsByClassName("songItem"));
 let songs = [
   {
     songName: "Hum Nashe Mein Toh Nahin",
-    filePath: "1.mp3",
-    coverPath: "1.jpg",
+    filePath: "assets/1.mp3",
+    coverPath: "assets/1.jpg",
     fileduration: "03:58",
-    artistName: "Arijit Singh, Tulsi Kumar",
+    artistName: "Arijit Singh",
   },
   {
     songName: "Kesariya",
-    filePath: "2.mp3",
-    coverPath: "2.jpg",
+    filePath: "assets/2.mp3",
+    coverPath: "assets/2.jpg",
     fileduration: "04:28",
     artistName: "Arijit Singh",
   },
   {
     songName: "Kitni Haseen Hogi",
-    filePath: "3.mp3",
-    coverPath: "3.jpg",
+    filePath: "assets/3.mp3",
+    coverPath: "assets/3.jpg",
     fileduration: "05:30",
     artistName: "Arijit Singh",
   },
   {
     songName: "Mere Dholna (Arijit Version)",
-    filePath: "4.mp3",
-    coverPath: "4.jpg",
+    filePath: "assets/4.mp3",
+    coverPath: "assets/4.jpg",
     fileduration: "06:04",
     artistName: "Arijit Singh",
   },
   {
     songName: "Srivalli",
-    filePath: "5.mp3",
-    coverPath: "5.jpg",
+    filePath: "assets/5.mp3",
+    coverPath: "assets/5.jpg",
     fileduration: "03:44",
     artistName: "Javed Ali",
   },
   {
     songName: "Fitoor",
-    filePath: "6.mp3",
-    coverPath: "6.jpg",
+    filePath: "assets/6.mp3",
+    coverPath: "assets/6.jpg",
     fileduration: "05:08",
-    artistName: "Arijit Singh, Neeti Mohan",
+    artistName: "Arijit Singh",
   },
   {
     songName: "Rasiya",
-    filePath: "7.mp3",
-    coverPath: "7.jpg",
+    filePath: "assets/7.mp3",
+    coverPath: "assets/7.jpg",
     fileduration: "04:25",
     artistName: "Tushar Joshi",
   },
   {
     songName: "Tere Hawaale",
-    filePath: "8.mp3",
-    coverPath: "8.jpg",
+    filePath: "assets/8.mp3",
+    coverPath: "assets/8.jpg",
     fileduration: "05:46",
     artistName: "Arijit Singh",
   },
   {
     songName: "Deva Deva",
-    filePath: "9.mp3",
-    coverPath: "9.jpg",
+    filePath: "assets/9.mp3",
+    coverPath: "assets/9.jpg",
     fileduration: "04:39",
     artistName: "Arijit Singh",
   },
   {
     songName: "La Ilhaj",
-    filePath: "10.mp3",
-    coverPath: "10.jpg",
+    filePath: "assets/10.mp3",
+    coverPath: "assets/10.jpg",
     fileduration: "02:03",
     artistName: "Arijit Singh",
   },
@@ -175,7 +175,7 @@ Array.from(document.getElementsByClassName("songimg")).forEach((element) => {
       el1 = document.getElementById(iconindex);
       el1.classList.remove("fa-circle-play");
       el1.classList.add("fa-pause-circle");
-      audioElement.src = `${songIndex}.mp3`;
+      audioElement.src = `assets/${songIndex}.mp3`;
       audioElement.currentTime = 0;
       playit();
       document.getElementsByClassName("songItem")[
@@ -190,7 +190,7 @@ Array.from(document.getElementsByClassName("songimg")).forEach((element) => {
       el1 = document.getElementById(iconindex);
       el1.classList.remove("fa-circle-play");
       el1.classList.add("fa-pause-circle");
-      audioElement.src = `${songIndex}.mp3`;
+      audioElement.src = `assets/${songIndex}.mp3`;
       audioElement.currentTime = 0;
       masterPlay.classList.remove("fa-circle-play");
       masterPlay.classList.add("fa-pause-circle");
@@ -224,7 +224,7 @@ Array.from(document.getElementsByClassName("songItemPlay")).forEach(
         iconindex = "i" + songIndex;
         e.target.classList.remove("fa-circle-play");
         e.target.classList.add("fa-pause-circle");
-        audioElement.src = `${songIndex}.mp3`;
+        audioElement.src = `assets/${songIndex}.mp3`;
         audioElement.currentTime = 0;
         playit();
         document.getElementsByClassName("songItem")[
@@ -239,7 +239,7 @@ Array.from(document.getElementsByClassName("songItemPlay")).forEach(
         gif.style.opacity = 1;
         e.target.classList.remove("fa-circle-play");
         e.target.classList.add("fa-pause-circle");
-        audioElement.src = `${songIndex}.mp3`;
+        audioElement.src = `assets/${songIndex}.mp3`;
         audioElement.currentTime = 0;
         masterPlay.classList.remove("fa-circle-play");
         masterPlay.classList.add("fa-pause-circle");
@@ -249,7 +249,6 @@ Array.from(document.getElementsByClassName("songItemPlay")).forEach(
             `${i}`
           ].style.backgroundColor = "rgb(40,40,40)";
         }
-        console.log(songIndex);
         document.getElementsByClassName("songItem")[
           `${songIndex - 1}`
         ].style.backgroundColor = "rgb(58,58,58)";
@@ -305,7 +304,7 @@ document.getElementById("next").addEventListener("click", () => {
     document.getElementById("duration-audio").innerHTML =
       songs[`${songIndex - 1}`].fileduration;
   }
-  audioElement.src = `${songIndex}.mp3`;
+  audioElement.src = `assets/${songIndex}.mp3`;
   audioElement.currentTime = 0;
   masterPlay.classList.remove("fa-circle-play");
   masterPlay.classList.add("fa-pause-circle");
@@ -352,7 +351,7 @@ document.getElementById("previous").addEventListener("click", () => {
       .classList.add("fa-pause-circle");
     songIndex -= 1;
   }
-  audioElement.src = `${songIndex}.mp3`;
+  audioElement.src = `assets/${songIndex}.mp3`;
   audioElement.currentTime = 0;
   masterPlay.classList.remove("fa-circle-play");
   masterPlay.classList.add("fa-pause-circle");
@@ -363,3 +362,20 @@ document.getElementById("previous").addEventListener("click", () => {
     songs[`${songIndex - 1}`].fileduration;
   gif.style.opacity = 1;
 });
+
+/* To open a new tab */
+function popitup(url) {
+  newwindow = window.open(
+    url,
+    "name",
+    "height=300,width=650,screenX=400,screenY=350"
+  );
+  if (window.focus) {
+    newwindow.focus();
+  }
+  return false;
+}
+
+function showerr() {
+  prompt("This", "That");
+}
